4b1b1dfd74177e30975d2f1f20aef0b5364d1c23
Thomas DuQuesnay <ttyo@users.noreply.github.com>
2023-09-07 02:10:19 +0800

    Update cli_commands.py

--- service/common/cli_commands.py
+++ service/common/cli_commands.py
@@ -8,27 +8,6 @@ from service.models import db
 from flask import Flask
 from service.models import Account
 
-# Define Shell Context Processor and Other Setup Code
-
-
-def make_shell_context():
-    return {"app": app, "db": db, "Account": Account}
-
-# Create the Flask Application Instance
-
-
-app = Flask(__name__)
-
-# Set up configuration, database connections, etc.
-app.config.from_pyfile("config.py")
-db.init_app(app)
-
-# Define your routes, views, and other Flask-related code here
-# ...
-
-if __name__ == "__main__":
-    app.run()
-
 ######################################################################
 # Command to force tables to be rebuilt
 # Usage:
