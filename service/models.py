f11e2ded117a47634c6ad372f0a19835261cd538
Thomas DuQuesnay <ttyo@users.noreply.github.com>
2023-09-07 01:46:59 +0800

    Update models.py

--- service/models.py
+++ service/models.py
@@ -12,23 +12,6 @@ from flask import Flask
 from service.models import Account
 from service.routes import app  # Import app from your routes file
 
-# Define Shell Context Processor and Other Setup Code
-def make_shell_context():
-    return {"app": app, "db": db, "Account": Account}
-
-# Create the Flask Application Instance
-app = Flask(__name__)
-
-# Set up configuration, database connections, etc.
-app.config.from_pyfile("config.py")
-db.init_app(app)
-
-# Define your routes, views, and other Flask-related code here
-# ...
-
-if __name__ == "__main__":
-    app.run()
-
 
 logger = logging.getLogger("flask.app")
 
