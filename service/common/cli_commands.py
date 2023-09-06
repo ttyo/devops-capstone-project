8c9de62f2854a6d6baf0400641d1e1df9e5903a2
Thomas DuQuesnay <ttyo@users.noreply.github.com>
2023-09-07 02:13:47 +0800

    Update cli_commands.py

--- service/common/cli_commands.py
+++ service/common/cli_commands.py
@@ -4,10 +4,6 @@ Flask CLI Command Extensions
 from service import app
 from service.models import db
 
-# Import necessary modules and define models (if any)
-from flask import Flask
-from service.models import Account
-
 ######################################################################
 # Command to force tables to be rebuilt
 # Usage:
