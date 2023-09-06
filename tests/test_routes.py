ba4940188641dee83e698795f6ce2062037c61a9
Thomas DuQuesnay <ttyo@users.noreply.github.com>
2023-09-06 19:18:31 +0800

    Update test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -177,10 +177,12 @@ class TestAccountService(TestCase):
         self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
   def test_unsupported_media_type(self):
-      """Test creating an account with an unsupported media type."""
-      response = self.client.post(
-          "/accounts",
-          data=json.dumps({"name": "John Doe"}),
-          content_type="text/plain",  # Set an unsupported media type here
-      )
-      self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
+    """Test creating an account with an unsupported media type."""
+    response = self.client.post(
+        "/accounts",
+        data=json.dumps({"name": "John Doe"}),
+        content_type="text/plain",  # Set an unsupported media type here
+    )
+    self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
+
+
