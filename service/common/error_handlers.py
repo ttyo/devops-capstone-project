5e6557d769919e999bd7c6fc57f3bff889a9ca78
Thomas DuQuesnay <ttyo@users.noreply.github.com>
2023-09-07 01:38:11 +0800

    Update error_handlers.py

--- service/common/error_handlers.py
+++ service/common/error_handlers.py
@@ -6,6 +6,27 @@ from service.models import DataValidationError
 from service import app
 from . import status
 
+# Import necessary modules and define models (if any)
+from flask import Flask
+from service.models import Account
+from service.routes import app  # Import app from your routes file
+
+# Define Shell Context Processor and Other Setup Code
+def make_shell_context():
+    return {"app": app, "db": db, "Account": Account}
+
+# Create the Flask Application Instance
+app = Flask(__name__)
+
+# Set up configuration, database connections, etc.
+app.config.from_pyfile("config.py")
+db.init_app(app)
+
+# Define your routes, views, and other Flask-related code here
+# ...
+
+if __name__ == "__main__":
+    app.run()
 
 ######################################################################
 # Error Handlers
